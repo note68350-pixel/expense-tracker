756398354b7ae830093bf321eb59685af997ca91
note68350-pixel <note68350@gmail.com>
2026-05-14 21:44:48 +0700

    first upload

---
+++ script.js
@@ -0,0 +1,99 @@
+let expenses = JSON.parse(localStorage.getItem("expenses")) || [];
+
+renderExpenses();
+
+function addExpense(){
+
+  const date = document.getElementById("date").value;
+  const title = document.getElementById("title").value;
+  const category = document.getElementById("category").value;
+  const amount = Number(document.getElementById("amount").value);
+
+  if(!date || !title || !amount){
+    alert("กรุณากรอกข้อมูลให้ครบ");
+    return;
+  }
+
+  const expense = {
+    id: Date.now(),
+    date,
+    title,
+    category,
+    amount
+  };
+
+  expenses.push(expense);
+
+  saveData();
+
+  renderExpenses();
+
+  clearForm();
+}
+
+function renderExpenses(){
+
+  const expenseList = document.getElementById("expenseList");
+
+  expenseList.innerHTML = "";
+
+  let total = 0;
+
+  expenses.forEach(expense => {
+
+    total += expense.amount;
+
+    const tr = document.createElement("tr");
+
+    tr.innerHTML = `
+      <td>${expense.date}</td>
+      <td>${expense.title}</td>
+      <td>${expense.category}</td>
+      <td>${expense.amount.toLocaleString()} บาท</td>
+      <td>
+        <button class="delete-btn" onclick="deleteExpense(${expense.id})">
+          ลบ
+        </button>
+      </td>
+    `;
+
+    expenseList.appendChild(tr);
+
+  });
+
+  document.getElementById("monthlyTotal").innerText =
+    `รวมเดือนนี้: ${total.toLocaleString()} บาท`;
+}
+
+function deleteExpense(id){
+
+  expenses = expenses.filter(expense => expense.id !== id);
+
+  saveData();
+
+  renderExpenses();
+}
+
+function saveData(){
+
+  localStorage.setItem("expenses", JSON.stringify(expenses));
+}
+
+function clearForm(){
+
+  document.getElementById("date").value = "";
+  document.getElementById("title").value = "";
+  document.getElementById("amount").value = "";
+}
+if ("serviceWorker" in navigator) {
+
+  window.addEventListener("load", () => {
+
+    navigator.serviceWorker.register("./service-worker.js")
+      .then(() => {
+        console.log("Service Worker Registered");
+      });
+
+  });
+
+}
